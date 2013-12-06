720a41e8bc38bbc9bdc33172b6f5364711d18004
Chris Montes <chrismontes@about.me>
2013-12-06 00:15:07 -0800

    Removes old documentation

--- lib/sasquatchjs.rb
+++ lib/sasquatchjs.rb
@@ -4,14 +4,9 @@ require 'quatch/quatcher'
 module Sasquatch
   # Sasquatch to file system modifications on a either single directory or multiple directories.
   #
-  # @param (see Listen::Listener#new)
+  # @param (see Sasquatch::Quatcher#new)
   #
-  # @yield [modified, added, removed] the changed files
-  # @yieldparam [Array<String>] modified the list of modified files
-  # @yieldparam [Array<String>] added the list of added files
-  # @yieldparam [Array<String>] removed the list of removed files
-  #
-  # @return [Listen::Listener] the listener
+  # @return [Sasquatch::Quatcher] the listener
   #
   def self.watch(*args, &block)
     Quatcher.new(*args, &block)
