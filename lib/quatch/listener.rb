e5a43ba73b4bdcff18e6fc24e75f0cf8f349c242
Chris Montes <chrismontes@about.me>
2013-12-10 02:12:56 -0800

    Moves listener functions into listener class

--- lib/quatch/listener.rb
+++ lib/quatch/listener.rb
@@ -3,5 +3,83 @@ require 'listen' # require listen gem
 module Sasquatch
   class Listener
     include Logger
+    attr_reader :file, :files, :listener, :status, :base_path, :file_changed
+
+    def initialize file, &block
+      @file = file
+
+      @base_path = File.dirname(@file)
+
+      validate_file @file
+
+      find_imports @file
+
+      @file_changed = block
+
+      # init the logger
+      logger "Watching '#{@file}' for updates"
+
+      # listen to the main file's directory
+      @listener = Listen.to(File.dirname(@file), only: /\.js$/, &hear)
+      # start listening
+      start
+    end
+
+    def find_imports file
+      @files = {}
+      File.read(file).scan(/\/\* @import (.*.js)/) do |filename|
+        @import = "#{File.dirname(@file)}/#{filename.first}"
+        validate_file @import
+        @files[filename.first] = File.new(File.absolute_path(@import))
+      end
+    end
+
+    def hear
+      Proc.new do |modified, added, removed|
+        if(modified)
+          modified_file = modified.first
+          if(imported? modified_file)
+            if(@file_changed)
+              @file_changed.call(_get_file_name(modified_file), @file, @files)
+            end
+          end
+        end
+      end
+    end
+
+    def start
+      @listener.start # not blocking
+      while( @running ) do
+        sleep(1)
+      end
+    end
+
+    def stop
+      @listener.pause
+      @running = false
+    end
+
+    def validate_file file
+      unless(File.file?(file))
+        raise "Specified file doesn't exist"
+      end
+    end
+
+    def imported? file
+
+      filename = _get_file_name file
+
+      if(filename == _get_file_name(@file) or @files.keys.include? filename)
+        return true
+      end
+
+      false
+
+    end
+
+    def _get_file_name filename
+      filename.sub(/^.*?#{@base_path}\//, '')
+    end
+
   end
 end
--- lib/quatch/quatcher.rb
+++ lib/quatch/quatcher.rb
@@ -7,18 +7,11 @@ require "optparse"
 
 module Sasquatch
   class Quatcher
-    attr_accessor :files, :status, :options
-    attr_reader :listener, :running
     include Logger
     def initialize *args
 
       @options = parse_options args
-      @running = true
 
-      @options[:files].each do |file|
-        unless(File.file?(file))
-          raise "Specified file doesn't exist"
-        end
 
         find_imports(file)
 
@@ -31,48 +24,11 @@ module Sasquatch
 
     end
 
-    def find_imports file
-      @files = {}
-      File.read(file).scan(/\/\* @import (.*.js)/) do |filename|
-        @files[filename.first] = File.absolute_path(filename.first)
+    end
+
+
+
       end
-    end
-
-    def listen dir
-      # setup the listener on the main file's directory
-      @listener = Listen.to(dir, only: /\.js$/) do |modified, added, removed|
-        file_changed modified
-        p @files
-        stop_listener
-      end
-      # start the listener
-      start_listener
-    end
-
-    def listener_callback
-      Proc.new do |modified, added, removed|
-        file_changed modified
-        p @files
-        stop_listener
-      end
-    end
-
-    def start_listener
-      # start listening
-      @listener.start # not blocking
-      while (@running)
-        sleep(2)
-      end
-    end
-
-    def stop_listener
-      @listener.stop
-      # @running = false
-    end
-
-    def file_changed
-
-      logger "modified absolute path: #{modified}"
     end
 
     def parse_options(argv)
