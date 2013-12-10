24603696a27c807f120c55f10cea9272a72b44a2
Chris Montes <chrismontes@about.me>
2013-12-10 02:17:05 -0800

    Adds compile method to compile specified files into the main file

--- lib/quatch/compiler.rb
+++ lib/quatch/compiler.rb
@@ -8,5 +8,21 @@ module Sasquatch
       # set the default extension for compiled files
       @ext = ext
     end
+
+    # compiles files into the specified file
+    def compile(files, file)
+
+      # setup the output buffer with the base file
+      output_buffer = File.read(File.path(file))
+
+      # loop through import files and substitute imports
+      files.each do |key, import_file|
+        output_buffer = import(output_buffer, key, import_file)
+      end
+
+      File.open(output_filename(file), 'w') do |file|
+        file.write(output_buffer)
+      end
+    end
   end
 end
