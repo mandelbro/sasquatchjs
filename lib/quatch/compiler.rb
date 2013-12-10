1e2a6971bba353aa2165d62c5339e39fe7b31148
Chris Montes <chrismontes@about.me>
2013-12-10 02:16:42 -0800

    Adds init function which sets the ext

--- lib/quatch/compiler.rb
+++ lib/quatch/compiler.rb
@@ -3,5 +3,10 @@ module Sasquatch
   class Compiler
     include Logger
     attr_accessor :ext
+
+    def initialize ext
+      # set the default extension for compiled files
+      @ext = ext
+    end
   end
 end
