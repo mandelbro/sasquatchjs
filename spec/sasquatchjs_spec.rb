f8a8d44229a4082adcad706e352560680f36cad3
Chris Montes <chrismontes@about.me>
2013-12-10 12:59:58 -0800

    Moving flushes around

--- spec/sasquatchjs_spec.rb
+++ spec/sasquatchjs_spec.rb
@@ -8,7 +8,6 @@ describe "SasquatchJS" do
       expect {
         @thread = Thread.new { Sasquatch.watch('spec/js/application.js') }
         @thread.abort_on_exception = true
-        STDOUT.flush
         sleep(1)
         @thread.exit
       }.to_not raise_exception
@@ -18,7 +17,6 @@ describe "SasquatchJS" do
       expect {
         @thread = Thread.new { Sasquatch.watch('spec/js/error') }
         @thread.abort_on_exception = true
-        STDOUT.flush
         sleep(1)
         @thread.exit
       }.to raise_exception
@@ -28,16 +26,15 @@ describe "SasquatchJS" do
       expect {
         @thread = Thread.new { Sasquatch.watch('spec/js/import_error.js') }
         @thread.abort_on_exception = true
-        STDOUT.flush
         sleep(1)
         @thread.exit
       }.to raise_exception
     end
     # it should tell me what file is being watched
     it "should tell me what file is being watched" do
+      STDOUT.flush
       @thread = Thread.new { Sasquatch.watch('spec/js/application.js') }
       STDOUT.should_receive(:puts).and_return("Watching 'spec/js/application.js' for updates")
-      STDOUT.flush
       @thread.abort_on_exception = true
       sleep(1)
       @thread.exit
@@ -55,21 +52,20 @@ describe "SasquatchJS" do
     sleep(1)
     @thread = Thread.new { Sasquatch.watch('spec/js/application.js') }
     @thread.abort_on_exception = true
-    STDOUT.flush
     # it should start listening to a valid file
     it "should listen to changes to the initilized file" do
       sleep(2)
+      STDOUT.flush
       File.write(f = 'spec/js/application.js', File.read(f).gsub(/\n.*line added by rspec|\n{1}\z/, "\n // #{Time.now} line added by rspec"))
       STDOUT.should_receive(:puts).and_return("Sasquatch has detected a change to spec/js/application.js, recompiling...")
-      STDOUT.flush
       sleep(2)
     end
     # should listen to changes to the imported files
     it "should listen to changes to the imported files" do
       sleep(2)
+      STDOUT.flush
       File.write(f = File.path('spec/js/test.js'), File.read(f).gsub(/\n.*line added by rspec|\n{1}\z/, "\n // #{Time.now} line added by rspec"))
       STDOUT.should_receive(:puts).and_return("Sasquatch has detected a change to spec/js/test.js, recompiling...")
-      STDOUT.flush
       sleep(2)
     end
     @thread.exit
